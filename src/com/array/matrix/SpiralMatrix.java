d7a0a83c8e38e4d5115c655a117b7bcacf0285ee
vdatla <vdatla@10.62.44.152>
2018-04-27 15:31:52 -0400

    Spiral Matrix

---
+++ src/com/array/matrix/SpiralMatrix.java
@@ -0,0 +1,61 @@
+package com.array.matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+	static int[][] matrix;
+	public static void main(String[] args) {
+		getSpiralMatrix(8);
+		List<Integer> list=fetchSpiralPattern(matrix);
+		System.out.println(list);
+	}
+private static List<Integer> fetchSpiralPattern(int[][] matrix) {
+		List<Integer> res=new ArrayList<>();
+		fetchSpiralPattern(matrix,matrix.length,0,matrix[0].length-1,res);
+		return res;
+	}
+private static void fetchSpiralPattern(int[][] matrix, int size,int start, int end, List<Integer> res) {
+	if(size==0) return;
+	for(int j=start;j<=end;j++) res.add(matrix[start][j]);
+	for(int i=start+1;i<=end;i++) res.add(matrix[i][end]);
+	for(int j=end-1;j>=start;j--) res.add(matrix[end][j]);
+	for(int i=end-1;i>start;i--) res.add(matrix[i][start]);
+	fetchSpiralPattern(matrix,size-1,start+1,end-1,res);
+}
+public static void getSpiralMatrix(int n) {
+	if(n<1) {
+		throw new IllegalArgumentException("n must be > 0");
+	}
+	matrix=new int[n][n];
+	fillSpiralMatrix(matrix,n,0,n-1,0);
+	prinMatrix(matrix);
+}
+
+private static void prinMatrix(int[][] matrix) {
+	for(int i=0;i<matrix.length;i++) {
+		for(int j=0;j<matrix[i].length;j++) {
+			System.out.print(matrix[i][j]+"  ");
+		}
+		System.out.println();
+	}
+	
+}
+
+private static void fillSpiralMatrix(int[][] matrix, int n, int start, int end,int val) {
+	if(n==0) return;
+	for(int j=start;j<=end;j++) {
+		matrix[start][j]=++val;
+	}
+	for(int i=start+1;i<=end;i++) {
+		matrix[i][end]=++val;
+	}
+	for(int j=end-1;j>=start;j--) {
+		matrix[end][j]=++val;
+	}
+	for(int i=end-1;i>start;i--) {
+		matrix[i][start]=++val;
+	}
+	fillSpiralMatrix(matrix,n-1,start+1,end-1,val);
+}
+}
